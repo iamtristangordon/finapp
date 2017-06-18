7856cc3510c5e15ffe9c65f3b047c179051272c8
Tristan Gordon <tristan@tdgordon.com>
2017-06-17 23:59:19 -0500

    Add login and signup system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './_guards/auth.guard';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent }  from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BudgetsComponent } from './budgets/budgets.component';
+import { GoalsComponent } from './goals/goals.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     { path: 'signup', component: SignupComponent },
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard] },
+    { path: 'budgets', component: BudgetsComponent, canActivate: [AuthGuard] },
+    { path: 'goals', component: GoalsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
