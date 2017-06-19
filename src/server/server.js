615b7d3a09b93c8b6585347908ddf5eda824e1f3
Tristan Gordon <tristan@tdgordon.com>
2017-06-19 16:53:40 -0500

    server fix

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/../../dist'));
 
 console.log(__dirname + '/../../dist');
 
