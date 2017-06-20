c896a356583321f057c04ec7b3669ad33f34074c
Tristan Gordon <tristan@tdgordon.com>
2017-06-19 21:16:43 -0500

    debug server url

--- src/app/_helpers/custom-http.ts
+++ src/app/_helpers/custom-http.ts
@@ -14,19 +14,19 @@ export class CustomHttp extends Http {
     }
  
     get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return super.get(appConfig.apiUrl + url, this.addJwt(options)).catch(this.handleError);
+        return super.get(url, this.addJwt(options)).catch(this.handleError);
     }
  
     post(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
-        return super.post(appConfig.apiUrl + url, body, this.addJwt(options)).catch(this.handleError);
+        return super.post(url, body, this.addJwt(options)).catch(this.handleError);
     }
  
     put(url: string, body: string, options?: RequestOptionsArgs): Observable<Response> {
-        return super.put(appConfig.apiUrl + url, body, this.addJwt(options)).catch(this.handleError);
+        return super.put(url, body, this.addJwt(options)).catch(this.handleError);
     }
  
     delete(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return super.delete(appConfig.apiUrl + url, this.addJwt(options)).catch(this.handleError);
+        return super.delete(url, this.addJwt(options)).catch(this.handleError);
     }
  
     // private helper methods
